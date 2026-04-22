7ec6d0dc6a355f5546bb07480f5023039218a6ce
Eric8cheese <huange@stu.otc.school.nz>
2026-04-22 15:15:51 +1200

    changes loop and finished the dict

--- NECEAL1.py
+++ NECEAL1.py
@@ -1,17 +1,25 @@
 '''This is a quiz on the movie "hunt for the wilderpeople" made by Eric Huang'''
 
-huntforthewilderpeopleQandA = {"What is the directors name?" : "Taika Waititi"}
-numofquestion = 14
+huntforthewilderpeopleQandA = {"What is the directors name?" : "Taika Waititi", "What type of poem does Ricky love to write?" : "Haiku", "What is the name of Ricky's foster father?" : "Uncle Hec", "One part of the chase sequence in the movie is very similar to an advertisement with Barry Crump in it. What brand of ute was the advertisement promoting?" : "Toyota", "What was the name of the man that Uncle Hec and Ricky hide out at?" : "Psycho Sam", "What is the name of the girl who finds Ricky" : "Kahu", "Who argue over who is more like the terminator?" : "Paula and Ricky", "What are names of the dogs?" : "Zag And Tupac", "What kind of chocolate bar is advertised?" : "Flake", "What is the name of the social welfare officer?" : "Paula"}
+numofquestion = 0
 usrinp = ""
 score = 0
 
-while numofquestion != 0:
+while numofquestion < 10:
     for question in huntforthewilderpeopleQandA.keys():
         print(question)
-        usrinp = input().title()
-        if question in huntforthewilderpeopleQandA.items():
-            print("good job its correct!")
-            score = score + 1
-        else:
-            print("idiot")
+        for name in huntforthewilderpeopleQandA.items():
+            usrinp = input()
+            usrinp.lower()
+            usrinp.title()
+            print(usrinp)
+            if usrinp in name:
+                print("good job its correct!")
+                score = score + 1
+                numofquestion = numofquestion + 1
+                break
+            else:
+                print("idiot")
+                numofquestion = numofquestion + 1
+                break
         
