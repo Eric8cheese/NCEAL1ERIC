9f713f1e0ab2c4b3d2b4213fe292b6498ee7571d
Eric8cheese <huange@stu.otc.school.nz>
2026-04-22 14:55:59 +1200

    add code that i made before

--- NECEAL1.py
+++ NECEAL1.py
@@ -1,1 +1,17 @@
-print("hello")
+'''This is a quiz on the movie "hunt for the wilderpeople" made by Eric Huang'''
+
+huntforthewilderpeopleQandA = {"What is the directors name?" : "Taika Waititi"}
+numofquestion = 14
+usrinp = ""
+score = 0
+
+while numofquestion != 0:
+    for question in huntforthewilderpeopleQandA.keys():
+        print(question)
+        usrinp = input().title()
+        if question in huntforthewilderpeopleQandA.items():
+            print("good job its correct!")
+            score = score + 1
+        else:
+            print("idiot")
+        
