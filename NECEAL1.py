dc51fcc8c49d49ddf8da2935e935e9542af7af08
Eric8cheese <huange@stu.otc.school.nz>
2026-04-24 09:59:49 +1200

    minor bug fixes

--- NECEAL1.py
+++ NECEAL1.py
@@ -1,6 +1,9 @@
 '''This is a quiz on the movie "hunt for the wilderpeople" made by Eric Huang'''
 
-#This part setup the questions and adds the consistant and temp varables, to change the questions change the dict, using anser:question.
+import time
+import os
+
+#This part sets up the questions and adds the consistant and temp varables, to change the questions change the dict, using anser:question.
 huntforthewilderpeopleQandA = {"What is the directors name?" : "taika waititi", "What type of poem does Ricky love to write?" : "haiku", "What is the name of Ricky's foster father?" : "uncle hec", "One part of the chase sequence in the movie is very similar to an advertisement with Barry Crump in it. What brand of ute was the advertisement promoting?" : "toyota", "What was the name of the man that Uncle Hec and Ricky hide out at?" : "psycho sam", "What is the name of the girl who finds Ricky" : "kahu", "Who argue over who is more like the terminator?" : "paula and ricky", "What are names of the dogs?" : "zag And tupac", "What kind of chocolate bar is advertised?" : "flake", "What is the name of the social welfare officer?" : "paula"}
 numofquesdone = 0
 usrinp = ""
@@ -22,10 +25,10 @@ while numofquesdone < TOTALQANDA:
         if usrinp in answer:
             print("good job its correct!")
             score = score + POINTS
-            numofquestion = numofquestion + 1
+            numofquesdone = numofquesdone + 1
         else:
             print(f"That is incorrect, the answer was {answer}")
-            numofquestion = numofquestion + 1
+            numofquesdone = numofquesdone + 1
 
 #this part of the code gives more "user friendly" output before giving the score
 print("Congratulations you finished the quiz!")
