1bd21067d8c298c7ca8a27e690321085d8f3d607
Eric8cheese <huange@stu.otc.school.nz>
2026-04-28 11:00:39 +1200

    fixed lists added some other fixes and tried to add some bounderys

--- NECEAL1.py
+++ NECEAL1.py
@@ -3,32 +3,54 @@
 import time
 import os
 
-#This part sets up the questions and adds the consistant and temp varables, to change the questions change the dict, using anser:question.
-huntforthewilderpeopleQandA = {"What is the directors name?" : "taika waititi", "What type of poem does Ricky love to write?" : "haiku", "What is the name of Ricky's foster father?" : "uncle hec", "One part of the chase sequence in the movie is very similar to an advertisement with Barry Crump in it. What brand of ute was the advertisement promoting?" : "toyota", "What was the name of the man that Uncle Hec and Ricky hide out at?" : "psycho sam", "What is the name of the girl who finds Ricky" : "kahu", "Who argue over who is more like the terminator?" : "paula and ricky", "What are names of the dogs?" : "zag And tupac", "What kind of chocolate bar is advertised?" : "flake", "What is the name of the social welfare officer?" : "paula"}
+#This part sets up the questions and adds the consistant and temp varables, to 
+# change the questions change the dict, using anser:question.
+
+huntforthewilderpeopleQandA = {"What is the directors name?" : "taika waititi", 
+                                "What type of poem does Ricky love to write?" : "haiku", 
+                                "What is the name of Ricky's foster father?" : "uncle hec", 
+                                "One part of the chase sequence in the movie is very similar to an advertisement with Barry Crump in it. What brand of ute was the advertisement promoting?" : "toyota", 
+                                "What was the name of the man that Uncle Hec and Ricky hide out at?" : "psycho sam", 
+                                "What is the name of the girl who finds Ricky" : "kahu", 
+                                "Who argue over who is more like the terminator?" : "paula and ricky", 
+                                "What are names of the dogs?" : "zag And tupac", 
+                                "What kind of chocolate bar is advertised?" : "flake", 
+                                "What is the name of the social welfare officer?" : "paula"
+                                }
 numofquesdone = 0
 usrinp = ""
 score = 0
 POINTS = 1
 TOTALQANDA = len(huntforthewilderpeopleQandA)
 
-#This part loops through the questions getting the user input and comparing it to the answers to see if its correct then giving "user friendly" output while keeping score
+#This part loops through the questions getting the user input and comparing it 
+# to the answers to see if its correct then giving "user friendly" output while 
+# keeping score and testing for expected bounderys
 while numofquesdone < TOTALQANDA:
     for question, answer in huntforthewilderpeopleQandA.items():
         print(question)
         usrinp = input()
-        usrinp = usrinp.lower()
+        wordcheck = len(usrinp)
 
-        #These print statments are for debug
-        #print(usrinp)
-        #print(answer)
-
-        if usrinp in answer:
-            print("good job its correct!")
-            score = score + POINTS
-            numofquesdone = numofquesdone + 1
-        else:
-            print(f"That is incorrect, the answer was {answer}")
-            numofquesdone = numofquesdone + 1
+        #The first if statment checks for people who just put lots of letters 
+        # and the secound if statment catches numbers
+        if wordcheck > 16:
+            print("What you have entered is too long! Please try again.")
+            usrinp = input()
+        else: 
+            numbercheck = usrinp.isnumeric()
+            if numbercheck == True:
+                print("Invalid input! Letters only!")
+                usrinp = input()
+            else:
+                usrinp = usrinp.lower()
+                if usrinp in answer:
+                    print("good job its correct!")
+                    score = score + POINTS
+                    numofquesdone = numofquesdone + 1
+                else:
+                    print(f"That is incorrect, the answer was {answer}")
+                    numofquesdone = numofquesdone + 1
 
 #this part of the code gives more "user friendly" output before giving the score
 print("Congratulations you finished the quiz!")
