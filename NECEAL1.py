53dd534ae6527ad141af22456cd31ad213afdefe
Eric8cheese <huange@stu.otc.school.nz>
2026-05-01 09:57:35 +1200

    Added more bounderys an minor fixes

--- NECEAL1.py
+++ NECEAL1.py
@@ -4,16 +4,15 @@ import time
 import os
 
 #This part sets up the questions and adds the consistant and temp varables, to 
-# change the questions change the dict, using anser:question.
-
+# change the questions change the dict, using question:answer.
 huntforthewilderpeopleQandA = {"What is the directors name?" : "taika waititi", 
                                 "What type of poem does Ricky love to write?" : "haiku", 
                                 "What is the name of Ricky's foster father?" : "uncle hec", 
                                 "One part of the chase sequence in the movie is very similar to an advertisement with Barry Crump in it. What brand of ute was the advertisement promoting?" : "toyota", 
                                 "What was the name of the man that Uncle Hec and Ricky hide out at?" : "psycho sam", 
                                 "What is the name of the girl who finds Ricky" : "kahu", 
-                                "Who argue over who is more like the terminator?" : "paula and ricky", 
-                                "What are names of the dogs?" : "zag And tupac", 
+                                "What is the name of the bad egg protagonist?" : "ricky", 
+                                "What is the name of Ricky’s dog?" : "tupac", 
                                 "What kind of chocolate bar is advertised?" : "flake", 
                                 "What is the name of the social welfare officer?" : "paula"
                                 }
@@ -22,6 +21,8 @@ usrinp = ""
 score = 0
 POINTS = 1
 TOTALQANDA = len(huntforthewilderpeopleQandA)
+#change this to change the word limit
+WORDLIMIT = 20
 
 #This part loops through the questions getting the user input and comparing it 
 # to the answers to see if its correct then giving "user friendly" output while 
@@ -35,16 +36,19 @@ while numofquesdone < TOTALQANDA:
         wordcheck = len(usrinp)
         numbercheck = usrinp.isnumeric()
 
-        #The first if statment checks for people who just put lots of letters 
-        # and the secound if statment catches numbers
-        while wordcheck > 12 or numbercheck == True:
-            print("What you have entered is too long! Please try again.")
+        #this while loop checks for bounderys then gives the usr the change to
+        #try again
+        while wordcheck > WORDLIMIT or numbercheck == True or usrinp == "" or usrinp.startswith('-'):
+            print("Invalid input! Please try again.")
+            time.sleep(1)
+            os.system('cls')
+            print(question)
             usrinp = input()
             wordcheck = len(usrinp)
             numbercheck = usrinp.isnumeric()
 
         usrinp = usrinp.lower()
-        if usrinp in answer:
+        if usrinp == answer:
             print("good job its correct!")
             score = score + POINTS
             numofquesdone = numofquesdone + 1
@@ -52,7 +56,10 @@ while numofquesdone < TOTALQANDA:
             print(f"That is incorrect, the answer was {answer}")
             numofquesdone = numofquesdone + 1
 
+        time.sleep(2)
+        os.system('cls')
+
 #this part of the code gives more "user friendly" output before giving the score
 print("Congratulations you finished the quiz!")
-print(f"The score you got was {score} points!")
+print(f"The score you got was {score} out of {TOTALQANDA}!")
 input("press enter to exit")
