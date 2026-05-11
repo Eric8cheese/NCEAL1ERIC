3f4db7d15bc932970e43aa39d4c3f3cfdd6809ed
Eric8cheese <huange@stu.otc.school.nz>
2026-05-11 12:55:51 +1200

    added accuracy test for miss spelt words and minor fixes

--- NECEAL1.py
+++ NECEAL1.py
@@ -2,14 +2,16 @@
 
 import time
 import os
+from fuzzywuzzy import process
+from fuzzywuzzy import fuzz
 
 #This part sets up the questions and adds the consistant and temp varables, to 
 # change the questions change the dict, using question:answer.
-huntforthewilderpeopleQandA = {"What is the directors name?" : "taika waititi", 
+huntforthewilderpeopleQandA = { "What does Ricky call the bush?" : "jungle", 
                                 "What type of poem does Ricky love to write?" : "haiku", 
                                 "What is the name of the girl who dies in juvey?" : "amber", 
                                 "One part of the chase sequence in the movie is very similar to an advertisement with Barry Crump in it. What brand of ute was the advertisement promoting?" : "toyota", 
-                                "What was the name of the man that Uncle Hec and Ricky hide out at?" : "psycho sam", 
+                                "Who plays the role of Ricky Baker?" : "julian", 
                                 "What is the name of the girl who finds Ricky" : "kahu", 
                                 "What is the name of the bad egg protagonist?" : "ricky", 
                                 "What is the name of Ricky’s dog?" : "tupac", 
@@ -21,9 +23,9 @@ usrinp = ""
 score = 0
 POINTS = 1
 TOTALQANDA = len(huntforthewilderpeopleQandA)
-#change this to change the word limit
-WORDLIMIT = 20
 QUIZNAME = "Hunt for the wilder people"
+#change this to change the word limit
+WORDLIMIT = 15
 
 #This part loops through the questions getting the user input and comparing it 
 # to the answers to see if its correct then giving "user friendly" output while 
@@ -31,39 +33,52 @@ QUIZNAME = "Hunt for the wilder people"
 print(f"welcome to this quiz on {QUIZNAME}!")
 while numofquesdone < TOTALQANDA:
     for question, answer in huntforthewilderpeopleQandA.items():
-        print(question)
+        print(question + " (one word answers)")
         usrinp = input()
 
         #these are for testing bounderys
         wordcheck = len(usrinp)
         numbercheck = usrinp.isnumeric()
         has_special = not usrinp.isalnum()
+        
 
-        #this while loop checks for bounderys then gives the usr the change to
-        #try again
+        #this while loop checks for bounderys then gives the usr the chance to
+        #try again if its correct it goes through a accuracy test
         while wordcheck > WORDLIMIT or numbercheck == True or usrinp == "" or usrinp.startswith('-') or has_special == True:
             print("Invalid input! Please try again.")
             time.sleep(1)
             os.system('cls')
-            print(question)
+            print(question + " (one word answers)")
             usrinp = input()
             wordcheck = len(usrinp)
             numbercheck = usrinp.isnumeric()
+            has_special = not usrinp.isalnum()
 
         usrinp = usrinp.lower()
-        if usrinp in answer and usrinp == answer:
+        #fuzz.ratio works by comparing the usrinp and answer and gives a 
+        # accuracy score, if its over 78 its correct.
+        score = fuzz.ratio(usrinp, answer)
+        if score > 78:
             print("good job its correct!")
             score = score + POINTS
             numofquesdone = numofquesdone + 1
         else:
+            print(score)
             print(f"That is incorrect, the answer was {answer}")
             numofquesdone = numofquesdone + 1
 
         time.sleep(2)
         os.system('cls')
 
-#this part of the code gives more "user friendly" output before giving the score
-print("Congratulations you finished the quiz!")
+#this part of the code gives more "user friendly" output depending on how well 
+# they did before giving the score
+if score < 4:
+    print("Did you even watch the movie?")
+elif score < 8:
+    print("Good job you did great")
+else:
+    print("GET A JOB")
+
 print(f"The score you got was {score} out of {TOTALQANDA}!")
 input("press enter to exit")
 
